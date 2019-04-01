9cb9d285a3efe22d33ad05464d2716dc1f434ef2
Lance Hilliard <lance@digitalcreations.cc>
2019-03-31 19:52:00 -0500

    fix browser location

--- Wally.WinForms/MainForm.Designer.cs
+++ Wally.WinForms/MainForm.Designer.cs
@@ -28,23 +28,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer30mins = new System.Windows.Forms.Timer(this.components);
             this.NowLabel = new System.Windows.Forms.Label();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.timer10mins = new System.Windows.Forms.Timer(this.components);
-            this.forecastUserControl1 = new Wally.WinForms.ForecastUserControl();
-            this.forecastUserControl2 = new Wally.WinForms.ForecastUserControl();
-            this.forecastUserControl3 = new Wally.WinForms.ForecastUserControl();
-            this.forecastUserControl4 = new Wally.WinForms.ForecastUserControl();
             this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.timer1sec = new System.Windows.Forms.Timer(this.components);
             this.timer5mins = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.BalancesLabel = new System.Windows.Forms.Label();
+            this.forecastUserControl1 = new Wally.WinForms.ForecastUserControl();
+            this.forecastUserControl2 = new Wally.WinForms.ForecastUserControl();
+            this.forecastUserControl3 = new Wally.WinForms.ForecastUserControl();
+            this.forecastUserControl4 = new Wally.WinForms.ForecastUserControl();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -53,18 +53,18 @@
             // 
             this.chart1.BackColor = System.Drawing.Color.Transparent;
             this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea10.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea10);
-            legend10.Name = "Legend1";
-            this.chart1.Legends.Add(legend10);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(919, 0);
             this.chart1.Name = "chart1";
-            series10.ChartArea = "ChartArea1";
-            series10.IsValueShownAsLabel = true;
-            series10.IsVisibleInLegend = false;
-            series10.Legend = "Legend1";
-            series10.Name = "Series1";
-            this.chart1.Series.Add(series10);
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(446, 588);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
@@ -88,13 +88,13 @@
             // 
             this.webBrowser1.AllowWebBrowserDrop = false;
             this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
-            this.webBrowser1.Location = new System.Drawing.Point(-200, -57);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.Margin = new System.Windows.Forms.Padding(0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
             this.webBrowser1.ScrollBarsEnabled = false;
-            this.webBrowser1.Size = new System.Drawing.Size(1180, 615);
+            this.webBrowser1.Size = new System.Drawing.Size(980, 558);
             this.webBrowser1.TabIndex = 3;
             this.webBrowser1.TabStop = false;
             this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
@@ -103,34 +103,6 @@
             // 
             this.timer10mins.Interval = 600000;
             this.timer10mins.Tick += new System.EventHandler(this.timer10mins_Tick);
-            // 
-            // forecastUserControl1
-            // 
-            this.forecastUserControl1.Location = new System.Drawing.Point(14, 559);
-            this.forecastUserControl1.Name = "forecastUserControl1";
-            this.forecastUserControl1.Size = new System.Drawing.Size(236, 240);
-            this.forecastUserControl1.TabIndex = 4;
-            // 
-            // forecastUserControl2
-            // 
-            this.forecastUserControl2.Location = new System.Drawing.Point(257, 559);
-            this.forecastUserControl2.Name = "forecastUserControl2";
-            this.forecastUserControl2.Size = new System.Drawing.Size(236, 240);
-            this.forecastUserControl2.TabIndex = 5;
-            // 
-            // forecastUserControl3
-            // 
-            this.forecastUserControl3.Location = new System.Drawing.Point(501, 559);
-            this.forecastUserControl3.Name = "forecastUserControl3";
-            this.forecastUserControl3.Size = new System.Drawing.Size(236, 240);
-            this.forecastUserControl3.TabIndex = 6;
-            // 
-            // forecastUserControl4
-            // 
-            this.forecastUserControl4.Location = new System.Drawing.Point(744, 559);
-            this.forecastUserControl4.Name = "forecastUserControl4";
-            this.forecastUserControl4.Size = new System.Drawing.Size(236, 240);
-            this.forecastUserControl4.TabIndex = 7;
             // 
             // webBrowser2
             // 
@@ -174,6 +146,34 @@
             this.BalancesLabel.TabIndex = 0;
             this.BalancesLabel.Text = "Evan: $42.00; Quinn: $86.00";
             // 
+            // forecastUserControl1
+            // 
+            this.forecastUserControl1.Location = new System.Drawing.Point(14, 559);
+            this.forecastUserControl1.Name = "forecastUserControl1";
+            this.forecastUserControl1.Size = new System.Drawing.Size(236, 240);
+            this.forecastUserControl1.TabIndex = 4;
+            // 
+            // forecastUserControl2
+            // 
+            this.forecastUserControl2.Location = new System.Drawing.Point(257, 559);
+            this.forecastUserControl2.Name = "forecastUserControl2";
+            this.forecastUserControl2.Size = new System.Drawing.Size(236, 240);
+            this.forecastUserControl2.TabIndex = 5;
+            // 
+            // forecastUserControl3
+            // 
+            this.forecastUserControl3.Location = new System.Drawing.Point(501, 559);
+            this.forecastUserControl3.Name = "forecastUserControl3";
+            this.forecastUserControl3.Size = new System.Drawing.Size(236, 240);
+            this.forecastUserControl3.TabIndex = 6;
+            // 
+            // forecastUserControl4
+            // 
+            this.forecastUserControl4.Location = new System.Drawing.Point(744, 559);
+            this.forecastUserControl4.Name = "forecastUserControl4";
+            this.forecastUserControl4.Size = new System.Drawing.Size(236, 240);
+            this.forecastUserControl4.TabIndex = 7;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -196,6 +196,7 @@
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
